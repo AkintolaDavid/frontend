4daa1b73513a29e951e254e8558286cf16d4f4db
AkintolaDavid <akintoladvaid66@gmail.com>
2024-07-06 04:02:31 +0100

    Fix path issue for Landingpage component

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { ContentPage } from "ContentPage/ContentPage";
-import { Popular_course } from "../Popular_course/Popular_course";
-import { Free_courses } from "../Free_courses/Free_courses";
-import { ShopContext } from "../Context/ShopContext";
+import { Popular_course } from "../../Popular_course/Popular_course";
+import { Free_courses } from "../../Free_courses/Free_courses";
+import { ShopContext } from "../../Context/ShopContext";
 import { useContext } from "react";
 export const Landingpage = () => {
   const userFullName = useContext(ShopContext);
